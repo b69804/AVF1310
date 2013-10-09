27ae5141a99b83484beda288b51dfc2f70ab429a
Matthew Ashton <mashton237@fullsail.com>
2013-10-09 19:36:18 -0400

    trying api calls

--- avfMainProject/platforms/ios/www/js/main.js
+++ avfMainProject/platforms/ios/www/js/main.js
@@ -1,3 +1,31 @@
 // Matthew Ashton
 // AVF 1310
 
+$("#breakingNewsStories").on("pageinit", function(){
+
+$(function(){
+  var url = "http://api.usatoday.com/open/articles/topnews?encoding=json;callback=jsoncallbackmethod;api_key=jvgkjm5kbvdnvsa7kzn34rna";
+  
+  $.getJSON(url, breakingNews);
+  
+});
+
+var breakingNews = function(bNews){
+
+    console.log();
+};
+
+});
+
+$("#instagram").on("pageinit", function(){
+    
+    $(function(){
+      var url = "https://api.instagram.com/v1/tags/soccer/media/recent?callback=?&amp;client_id=46162f299ffe415fb8ba88bde15105fb&amp";
+      $.getJSON(url, instaSports);
+                     
+      });
+    
+     var instaSports = function(utdPics){
+        console.log(utdPics);
+     };
+});
