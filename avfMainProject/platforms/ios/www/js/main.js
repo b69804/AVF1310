bcac311afca0f00fb00163fc91f3657fba082e48
Matthew Ashton <mashton237@fullsail.com>
2013-10-09 23:24:55 -0400

    Fixed API calls, they now work

--- avfMainProject/platforms/ios/www/js/main.js
+++ avfMainProject/platforms/ios/www/js/main.js
@@ -3,42 +3,31 @@
 
 $("#breakingNewsStories").on("pageinit", function(){
 
-$(function(){
-  //var url = "http://api.usatoday.com/open/articles/topnews?api_key=jvgkjm5kbvdnvsa7kzn34rna";
+  var url = "http://api.espn.com/v1/now?limit=3&apikey=x24c8a9fbdsykefrd6jrfagw";
   
-  //$.getJSON(url, breakingNews);
-  $.ajax({
-         url: "http://api.usatoday.com/open/articles/topnews?api_key=jvgkjm5kbvdnvsa7kzn34rna",
-         type: "GET",
-         dataType: "json",
-         success: function(json){
-         console.log(json);
-            var items = $(json);
-                items.find("item").each(function(){
-                        var item = $(this);
-                                        $("");
-                                        
-                                        }
-         });
-  
+  $.getJSON(url, breakingNews);
 });
-
 var breakingNews = function(bNews){
 
-    console.log();
-};
+  $.each(bNews.feed, function(index, news){
+         var story = "<li class='ui-li ui-li-static ui-btn-up-a ui-first-child ui-last-child'><h3>" + news.headline + "</h3><p>"+ news.description +"</p></li>";
+         $('#news').append(story);
+         })
+    };
 
-});
+
+
 
 $("#instagram").on("pageinit", function(){
     
-    $(function(){
-      var url = "https://api.instagram.com/v1/tags/soccer/media/recent?callback=?&amp;client_id=46162f299ffe415fb8ba88bde15105fb";
+      var url = "https://api.instagram.com/v1/tags/manchesterunited/media/recent?callback=?&amp;client_id=46162f299ffe415fb8ba88bde15105fb";
       $.getJSON(url, instaSports);
-                     
-      });
-    
-     var instaSports = function(utdPics){
-        console.log(utdPics);
-     };
+                   
 });
+
+var instaSports = function(utdPics){
+    $.each(utdPics.data, function(index, photo){
+           var pics = "<li class='ui-li ui-li-static ui-btn-up-a ui-first-child ui-last-child' ><img src='" + photo.images.standard_resolution.url + "'height='100%' width='100%'></li>";
+           $("#unitedPhotos").append(pics);
+           });
+};
